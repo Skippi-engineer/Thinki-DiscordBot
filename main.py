aaa20952c079e2268fd9bd4a6c6e61d9af64ebf5
Skippi-engineer <83069081+Skippi-engineer@users.noreply.github.com>
2022-01-15 11:47:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import discord
 import os
 import random
 
+from discord.ext import commands
 from keep_alive import keep_alive
 
+# Global:
 CLIENT = discord.Client()
 PREFIX = "!"
 
 
 # Commands:
 def random_generator(first, last):
-  rand = random.randint(first, last)
-  return rand
+  return (random.randint(first, last))
+
+
+def losuj(message):
+  MSG = message.content
+  try:
+    last = int(MSG.split(PREFIX + "losuj", 1)[1])
+    return ("<:THO_BlobCatRobot:541708128794705996> **| Losowy numer od 1 do " + str(last) + " to: " + str(random_generator(1, last)) + "**")
+  except Exception:
+    return ("<:THO_BlobCatMage:736568215051829289> **| Poprawne uzycie to: `" + PREFIX + "losuj [przedzial]`**")
 
 
 # Working:
@@ -23,22 +33,29 @@ async def on_ready():
 # Waiting for the commands:
 @CLIENT.event
 async def on_message(message):
+  USERNAME = str(message.author).split('#'[0])
+  CHANNEL = str(message.channel.name)
+  USER_MESSAGE = str(message.content)
   MSG = message.content
   RESEND = message.channel.send
 
   if message.author == CLIENT.user:
     return
 
-  elif MSG.startswith(PREFIX + "hej"):
+  if message.channel.name != "komendy" and message.channel.name != "boty":
+    return
+
+  print(f"{USERNAME}: {USER_MESSAGE} ({CHANNEL})")
+
+  if MSG.startswith(PREFIX + "hej"):
     await RESEND("Hejka !")
+    return
 
   elif MSG.startswith(PREFIX + "losuj"):
-    try:
-      last = int(MSG.split(PREFIX + "losuj", 1)[1])
-      await RESEND("<:THO_BlobCatRobot:541708128794705996> **| Losowy numer od 1 do " + str(last) + " to: __" + str(random_generator(1, last)) + "__**")
-    except Exception:
-      await RESEND("<:THO_BlobCatMage:736568215051829289> **| Poprawne uzycie to: `" + PREFIX + "losuj [przedzial]`**" )
+    await RESEND(losuj(message))
+    return
 
 # Main:
-keep_alive()
-CLIENT.run(os.environ['TOKEN'])
+if __name__ == '__main__':
+  keep_alive()
+  CLIENT.run(os.environ['TOKEN'])
