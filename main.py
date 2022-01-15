8a64f292dabe76128bd7bfcf38bd26b3fc24959d
Skippi-engineer <83069081+Skippi-engineer@users.noreply.github.com>
2022-01-15 17:20:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,56 +6,70 @@ from discord.ext import commands
 from keep_alive import keep_alive
 
 # Global:
-CLIENT = discord.Client()
-PREFIX = "!"
+BOT = commands.Bot(command_prefix='!')
 
-
-# Commands:
+####################################################
+## Commands:
 def random_generator(first, last):
   return (random.randint(first, last))
 
-
-def losuj(message):
-  MSG = message.content
+# Losuj:
+@BOT.command()
+async def losuj(ctx, *args):
   try:
-    last = int(MSG.split(PREFIX + "losuj", 1)[1])
-    return ("<:THO_BlobCatRobot:541708128794705996> **| Losowy numer od 1 do " + str(last) + " to: " + str(random_generator(1, last)) + "**")
+    if len(args)==1:
+      first = 1
+      last = int(args[0])
+    elif len(args)==2:
+      first = int(args[0])
+      last = int(args[1])
+    else:
+      raise Exception
+
+    await ctx.send("<:THO_BlobCatRobot:541708128794705996> **| Losowy numer od "+ str(first) + " do " + str(last) + " to: " + str(random_generator(first, last)) + "**")
+
   except Exception:
-    return ("<:THO_BlobCatMage:736568215051829289> **| Poprawne uzycie to: `" + PREFIX + "losuj [przedzial]`**")
+    await ctx.send("<:THO_BlobCatMage:736568215051829289> **| Poprawne uzycie to: `" + BOT.command_prefix + "losuj [przedzial]`**")
 
 
-# Working:
-@CLIENT.event
+# Test:
+@BOT.command()
+async def test(ctx, *args):
+  await ctx.send(" ".join(args))
+
+
+# Ping
+@BOT.command()
+async def ping(ctx):
+  #await ctx.send("Pong! " + str(round(BOT.latency*1000)) + " ms!")
+  await ctx.send(f"Pong! {round(BOT.latency*1000)} ms!")
+
+
+####################################################
+## Working:
+@BOT.event
 async def on_ready():
-  print('Dzialam jako {0.user}.'.format(CLIENT))
+  print('Dzialam jako {0.user}.'.format(BOT))
 
 
-# Waiting for the commands:
-@CLIENT.event
-async def on_message(message):
-  USERNAME = str(message.author).split('#'[0])
-  CHANNEL = str(message.channel.name)
-  USER_MESSAGE = str(message.content)
-  MSG = message.content
-  RESEND = message.channel.send
+#@BOT.event
+#async def on_message(message):
+#  USERNAME = str(message.author).split('#'[0])
+#  CHANNEL = str(message.channel.name)
+#  USER_MESSAGE = str(message.content)
+#  MSG = message.content
+#  RESEND = message.channel.send
+#
+#  if message.author == BOT.user:
+#    return
+#
+#  if message.channel.name != "komendy" and message.channel.name != "boty":
+#    return
+#
+#  print(f"{USERNAME}: {USER_MESSAGE} ({CHANNEL})")
 
-  if message.author == CLIENT.user:
-    return
-
-  if message.channel.name != "komendy" and message.channel.name != "boty":
-    return
-
-  print(f"{USERNAME}: {USER_MESSAGE} ({CHANNEL})")
-
-  if MSG.startswith(PREFIX + "hej"):
-    await RESEND("Hejka !")
-    return
-
-  elif MSG.startswith(PREFIX + "losuj"):
-    await RESEND(losuj(message))
-    return
 
 # Main:
 if __name__ == '__main__':
   keep_alive()
-  CLIENT.run(os.environ['TOKEN'])
+  BOT.run(os.environ['TOKEN'])
